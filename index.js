a1289f70fa054c2ab46ddd45b8c5b8ab0224c7f4
Phil <phl.j.gray@gmail.com>
2022-07-17 14:18:29 -0700

    All scripts added& alternating text and img questions

--- index.js
+++ index.js
@@ -1,86 +1,78 @@
 "use strict";
 
-//APP FLOW
-//Letter is matched with button options available
-
-//If selection is correct...
-//1)User is notified correct
-//2) New Letter is genereated
-//3) Score is +1
-
-//If Letter is  is not correct
-//1) certain options are removed
-//2)Core is -1
-
 //letter objects
 const letters = [
   "ba",
-  // i: imgs/ba.svg
-
+  "bi",
+  "bu",
+  "ba",
+  "be",
+  "b",
   "ga",
-  // i: imgs/ga.svg
-
+  "g",
+  "gi",
+  "gu",
+  "go",
+  "ge",
   "nga",
-  // i: imgs/nga.svg
-
+  "k",
+  "ku",
+  "ki",
+  "ke",
   "ka",
-  // i: imgs/ka.svg
-
   "sa",
-  // i: imgs/sa.svg
-
-  "da_ra",
-  // i: imgs/da_ra.svg
-
+  "h",
   "ha",
-  // i: imgs/ha.svg
-
-  "i_e",
-  // i: imgs/i_e.svg
-
+  "he",
+  "hi",
+  "hu",
+  "ho",
+  "m",
   "ma",
-  // i: imgs/ma.svg
-
+  "me",
+  "mi",
+  "mu",
+  "mo",
+  "n",
   "na",
-  // i: imgs/na.svg
-
+  "ne",
+  "ni",
+  "nu",
+  "no",
+  "p",
   "pa",
-  // i: imgs/pa.svg
-
+  "pe",
+  "pi",
+  "pu",
+  "po",
+  "t",
+  "te",
+  "ti",
+  "to",
+  "tu",
   "ta",
-  // i: imgs/ta.svg
-
-  "u_o",
-  // i: imgs/u_o.svg
-
+  "w",
   "wa",
-  // i: imgs/wa.svg
-
+  "we",
+  "wi",
+  "wu",
+  "wo",
+  "y",
   "ya",
-  // i: imgs/ya.svg
+  "ye",
+  "yi",
+  "yu",
+  "yo",
+  "da_ra",
+  "i_e",
+  "u_o"
 ];
-
-// const letters = [
-//   ba,
-//   ga,
-//   nga,
-//   ka,
-//   sa,
-//   da_ra,
-//   ha,
-//   i_e,
-//   ma,
-//   na,
-//   pa,
-//   ta,
-//   u_o,
-//   wa,
-//   ya,
-// ];
 
 const qBox = document.querySelector(".question__box");
 const qImg = document.querySelector(".question__box--img");
 const qBtn = document.querySelector(".question__btn");
+const svgImg = document.getElementsByTagName("img")
+
 
 
 const aList = document.querySelector(".answer");
@@ -93,20 +85,21 @@ let aArray = [];
 
 let aBtnKey, aBtnHTML ,imgNode, currentValue, qImgNodes, currentScore;
 
-console.log(aBtn);
-
 //Toggle dark mode
 const toggleDark = function() {
   var element = document.body;
-  qBtn.classList.toggle("dark-mode")
-  // aBtn.forEach(i => {
+  qBtn.classList.toggle("dark-mode");
+  // svgImg.classList.toggle("dark-mode");
+  // console.log(aBtnArr);
+  
+  // aList.children.forEach(i => {
+  //   console.log(aBtn[i]);
   //   console.log(`for each`);
     
-  //   // i.classList.toggle("dark-mode")
+  //   i.classList.toggle("dark-mode")
   // });
   element.classList.toggle("dark-mode");
   console.log('checked');
-  
 }
 
 // toggleBtn.addEventListener('checked', toggleDark())
@@ -127,8 +120,15 @@ scoreBoard.textContent = currentScore;
 
 //Randomly selects from an array
 const randomSelect = function (arr) {
-  return Math.round(Math.random() * arr.length);
+  return Math.round(Math.random() * (arr.length-1));
 };
+
+const qType = ["txt", "img"];
+
+let qChoice = randomSelect(qType)
+let qCurrent = qType[qChoice];
+
+console.log(qCurrent);
 
 //Shuffles array
 function shuffle(arr) {
@@ -146,9 +146,7 @@ function shuffle(arr) {
       arr[randomIndex],
       arr[currentIndex],
     ];
-  }
-  console.log(arr);
-  
+  }  
   return arr;
 }
 
@@ -182,7 +180,11 @@ sessionStorage.setItem("currentScore", `${stringScore}`);
 const buildAnswerBtn = function(){
   aArray.forEach(i => {
     var button = document.createElement("button");
-    button.innerText = i;
+    if(qCurrent === "txt" ){
+      button.innerHTML = `<img src="imgs/${i}.svg" class="svg" ></img>`;
+    } else if (qCurrent === "img"){ 
+      button.innerText = i;
+    }
     button.className = "answer__btn"
     button.addEventListener("click", function(){
       matchAnswer(i);
@@ -193,30 +195,29 @@ const buildAnswerBtn = function(){
 
 currentValue = letters[randomLetters];
 
-//Generates question letter on click
+// //Generates question letter on click
 const generateQuestionLetter = function () {
   //Selects a random letter from the letters array
   randomLetters = randomSelect(letters);
   currentValue = letters[randomLetters];
-    // qImgNodes = qImg.childNodes[0];
-    // console.log(qImgNodes);
-    // imgNode = document.createElement("div");
-    // imgNode.className = "question__box--letter";
-    // qImg.replaceChild(imgNode, qImgNodes);
-    qImg.innerHTML = `<img src="imgs/${currentValue}.svg" height="100px"></img>`;
+  if(qCurrent === "img" ){
+    qImg.innerHTML = `<img src="imgs/${currentValue}.svg" class="svg" height="100px"></img>`;
+    console.log(`is img`);
+    
+  } else if (qCurrent === "txt") {
+    qImg.innerHTML = `${currentValue}`
+    console.log(`is txt`);
+  }
     console.log(`current value: ${currentValue}`);
     aArray.push(`${currentValue}`);
     };
   
-
   //Generates answer buttons of all the letters
   const generateAnswer = function () {
     shuffle(letters);
     for (let i = 0; i < 3; i++) {
       let x = letters[i];
-      if (x == currentValue ) {continue;}
-      console.log(x);
-      
+      if (x == currentValue ) {continue;}      
       aArray.push(`${x}`)
       
     }
@@ -231,6 +232,9 @@ const generateQuestionLetter = function () {
   const replaceAnswerBtn = function() {
     aArray = [];
     currentValue = letters[randomLetters];
+     qChoice = randomSelect(qType)
+     qCurrent = qType[qChoice];
+    console.log(`replace with ${qCurrent}`);
     // aList.(aBtn);
     while(aList.hasChildNodes()){
       aList.removeChild(aList.firstChild);
@@ -239,12 +243,10 @@ const generateQuestionLetter = function () {
     generateQuestionLetter()
     shuffle(aArray);
     buildAnswerBtn();
-    
-  
   }
 
-    //New letter generated
-//Generate new icon on "new" btn click"
+//     //New letter generated
+// //Generate new icon on "new" btn click"
 qBtn.addEventListener("click", replaceAnswerBtn);
 
 let stringScore = new String(currentScore);
--- test.js
+++ test.js
@@ -1,98 +1,62 @@
 "use strict"
 
-let aArray = [];
-let aBtnKey;
-let aBtnHTML;
-let aBtn = document.getElementsByClassName("answer__btn");
-let imgNode;
+const qType = ["txt", "img"];
 
-// let aBtnText = aBtn.textContent
+const qChoice = randomSelect(qType)
+const qCurrent = qType[qChoice];
 
-  //Matches question with answer on click
-  const matchAnswer = function (v) {
-    if (currentValue == v) {
-      //adds 1 point to current score if correct
-      currentScore = currentScore + 1;
-      //currentscore is entered into scoreboard
-      scoreBoard.textContent = currentScore;
-      // replaceAnswerBtn()  
-      replaceAnswerBtn()      
-      } else {
-        //deducts 1 point to current score if correct
-        currentScore = currentScore - 1;
-        //currentscore is entered into scoreboard
-        scoreBoard.textContent = currentScore;
-        console.log(v);
-      }
-  
-  let stringScore = new String(currentScore);
-  //set currentScore to session storage and turn it into a string
-  sessionStorage.setItem("currentScore", `${stringScore}`);
-};
+console.log(qCurrent);
 
-// Adds text content & eventlistner to all 4 answer buttons
+
+
 const buildAnswerBtn = function(){
   aArray.forEach(i => {
     var button = document.createElement("button");
-    button.innerText = i;
+    if(qCurrent === "txt" ){
+      button.innerHTML = `<img src="imgs/${i}.svg" class="svg" ></img>`;
+    } else if (qCurrent === "img"){ 
+      button.innerText = i;
+    }
+    button.className = "answer__btn"
     button.addEventListener("click", function(){
       matchAnswer(i);
     })
     aList.appendChild(button)
   })
 }
-
-currentValue = letters[randomLetters];
-
-//Generates question letter on click
-const generateQuestionLetter = function () {
-  currentValue = letters[randomLetters];
-    qImgNodes = qImg.childNodes[0];
-    imgNode = document.createElement("div");
-    //Selects a random letter from the letters array
-    randomLetters = randomSelect(letters);
-    imgNode.className = "question__box--letter";
-    qImg.replaceChild(imgNode, qImgNodes);
-    imgNode.innerHTML = `<img src="imgs/${currentValue}.svg" height="100px"></img>`;
-    console.log(`current value: ${currentValue}`);
-    aArray.push(`${currentValue}`);
-    };
-  
-  
-  //Generates answer buttons of all the letters
-  const generateAnswer = function () {
-    shuffle(letters);
-    for (let i = 0; i < 3; i++) {
-      let x = letters[i];
-      if (x == currentValue ) {continue;}
-      aArray.push(`${x}`)
-    }
-  };
+// //Generates question letter on click
+    const generateQuestionLetter = function () {
+      //Selects a random letter from the letters array
+      randomLetters = randomSelect(letters);
+      currentValue = letters[randomLetters];
+      if(qCurrent === "img" ){
+        qImg.innerHTML = `<img src="imgs/${currentValue}.svg" class="svg" height="100px"></img>`;
+        console.log(`is img`);
+        
+      } else if (qCurrent === "txt") {
+        qImg.innerHTML = `${currentValue}`
+        console.log(`is txt`);
+      }
+        console.log(`current value: ${currentValue}`);
+        aArray.push(`${currentValue}`);
+        };
+    //   //Generates answer buttons of all the letters
+    //   const generateAnswer = function () {
+//     shuffle(letters);
+//     for (let i = 0; i < 3; i++) {
+//       let x = letters[i];
+//       if (x == currentValue ) {continue;}      
+//       aArray.push(`${x}`)
+      
+//     }
+//   };
 
   generateQuestionLetter();
-  console.log(`current answer ${currentValue}`);
   
   generateAnswer();
-  console.log(`current answer ${currentValue}`);
+  
   buildAnswerBtn();
  
-//file:///run/media/phil/WD%20external/Code-projects/W3Schools-2021-09-19/js/js_promise.html
-// let loadPromise = new Promise(function(myResolve, myReject){
-//   aArray.forEach(i => {
-//     if (aArray[i] != undefined){
-//       myResolve(`${i} loaded`);      
-//     } else {
-//       myReject(`node ${i} not loaded`);
-//     }}
-//     )
-    
-//   })
-// loadPromise.then(
-//   function(value) {console.log(value);},
-//   function(error) {console.log(error);}
-// );
-
-  
   const replaceAnswerBtn = function() {
     aArray = [];
     currentValue = letters[randomLetters];
@@ -100,21 +64,12 @@ const generateQuestionLetter = function () {
     while(aList.hasChildNodes()){
       aList.removeChild(aList.firstChild);
     }
-    generateQuestionLetter()
     generateAnswer()
+    generateQuestionLetter()
     shuffle(aArray);
     buildAnswerBtn();
     
   
   }
-
-  
-  
-  //New letter generated
-//Generate new icon on "new" btn click"
 qBtn.addEventListener("click", replaceAnswerBtn);
 
-let stringScore = new String(currentScore);
-
-//set currentScore to session storage and turn it into a string
-sessionStorage.setItem("currentScore", `${stringScore}`);
