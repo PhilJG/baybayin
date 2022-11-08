febd35d4c5bbd39dc97ae4fa337a8e812c1cc5c6
Phil <phl.j.gray@gmail.com>
2022-11-08 14:15:30 -0800

    trying to fix typerror

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-"use strict";
+'use strict';
 
-const qBox = document.querySelector(".question__box");
-const qImg = document.querySelector(".question__box--img");
-const qSeq = document.querySelector(".question__box--sequence");
-const qBtn = document.querySelector(".question__btn");
-const svgImg = document.getElementsByTagName("svg");
+const qBox = document.querySelector('.question__box');
+const qImg = document.querySelector('.question__box--img');
+const qSeq = document.querySelector('.question__box--sequence');
+const qBtn = document.querySelector('.question__btn');
+const svgImg = document.getElementsByTagName('svg');
 
-const aList = document.querySelector(".answer");
-let aBtn = document.getElementsByClassName("answer__btn");
-const scoreBoard = document.querySelector(".score__points");
-const scorePoints = document.querySelector(".score__points--inc");
-const toggleBtn = document.querySelector(".switch");
+const aList = document.querySelector('.answer');
+let aBtn = document.getElementsByClassName('answer__btn');
+const scoreBoard = document.querySelector('.score__points');
+const scorePoints = document.querySelector('.score__points--inc');
+const toggleBtn = document.querySelector('.switch');
 
-const mBtn = document.querySelector("#myBtn");
+const mBtn = document.querySelector('#myBtn');
 
 const allBtns = [mBtn, aBtn, qBtn];
 
@@ -23,7 +23,7 @@ let aBtnKey, aBtnHTML, imgNode, currentValue, qImgNodes, currentScore;
 //get sessionStorage of previous current score on refresh and turn it into a number
 function getScore() {
   if (localStorage.length > 0) {
-    currentScore = ~~localStorage.getItem("currentScore");
+    currentScore = ~~localStorage.getItem('currentScore');
   } else {
     currentScore = 0;
   }
@@ -38,7 +38,7 @@ const randomSelect = function (arr) {
   return Math.round(Math.random() * (arr.length - 1));
 };
 
-const qType = ["txt", "img"];
+const qType = ['txt', 'img'];
 
 let qChoice = randomSelect(qType);
 let qCurrent = qType[qChoice];
@@ -117,11 +117,11 @@ const matchAnswer = function (v) {
     //currentscore is entered into scorePoints
     scorePoints.textContent = currentScore;
     qSeq.textContent = currentValue.sequence;
-    scoreBoard.classList.add("correct");
-    qSeq.classList.add("correct");
+    scoreBoard.classList.add('correct');
+    qSeq.classList.add('correct');
     setTimeout(function () {
       replaceAnswerBtn();
-      qSeq.classList.remove("correct");
+      qSeq.classList.remove('correct');
     }, 1000);
   } else {
     //deducts 1 point to current score if correct
@@ -130,11 +130,11 @@ const matchAnswer = function (v) {
     currentValue.sequence = 0;
     //currentscore is entered into scorePoints
     scorePoints.textContent = currentScore;
-    scoreBoard.classList.add("incorrect");
-    qSeq.classList.add("incorrect");
+    scoreBoard.classList.add('incorrect');
+    qSeq.classList.add('incorrect');
     setTimeout(function () {
       scoreBoard.classList.remove();
-      qSeq.classList.remove("incorrect");
+      qSeq.classList.remove('incorrect');
       replaceAnswerBtn();
     }, 1000);
   }
@@ -144,21 +144,21 @@ const matchAnswer = function (v) {
   setReviewDate(currentValue);
 
   //set currentScore to session storage and turn it into a string
-  localStorage.setItem("currentScore", `${stringScore}`);
-  localStorage.setItem("reviewedLetters", JSON.stringify(reviewedLetters));
+  localStorage.setItem('currentScore', `${stringScore}`);
+  localStorage.setItem('reviewedLetters', JSON.stringify(reviewedLetters));
 };
 
 // Adds text content & eventlistner to all 4 answer buttons
 const buildAnswerBtn = function () {
   aArray.forEach((i) => {
-    var button = document.createElement("button");
-    if (qCurrent === "txt") {
+    var button = document.createElement('button');
+    if (qCurrent === 'txt') {
       button.innerHTML = `<img src="imgs/${i}.svg" class="svg" ></img>`;
-    } else if (qCurrent === "img") {
+    } else if (qCurrent === 'img') {
       button.innerText = i;
     }
-    button.className = "answer__btn";
-    button.addEventListener("click", function () {
+    button.className = 'answer__btn';
+    button.addEventListener('click', function () {
       matchAnswer(i);
     });
     aList.appendChild(button);
@@ -168,19 +168,35 @@ const buildAnswerBtn = function () {
 //Generates question letter on click
 const generateQuestionLetter = function () {
   let r = reviewedLetters.filter((x) => x.reviewed === false);
-  console.log(r);
 
   //Selects a random letter from the r array
   randomLetters = randomSelect(r);
 
   currentValue = r[randomLetters];
   console.log(currentValue);
+  console.log(qCurrent);
 
-  if (qCurrent === "img") {
+  if (qCurrent === 'img') {
     qImg.innerHTML = `<img src="imgs/${currentValue.text}.svg" class="svg" height="100px">`;
-  } else if (qCurrent === "txt") {
+  } else if (qCurrent === 'txt') {
     qImg.innerHTML = `${currentValue.text}`;
+  } else if (qCurrent != 'img' || 'text') {
+    qImg.innerHTML =
+      'All the letters have been reviewed for today. Try learning some new ones! ';
   }
+
+  // try {
+  //   console.log('error test begins');
+  //   unicycle;
+
+  //   console.log('End of try runs -- never reached');
+  //   // if(currentValue = undefined) {
+  //   //   throw new Error('All your previous letters have been reviewed for today. Try learning some new letters');
+  //   // } catch(e) {
+  //   //   console.log(e);
+  // } catch (err) {
+  //   console.log('Error has occured: ' + err);
+  // }
   // post letter sequence
   qSeq.innerHTML = currentValue.sequence;
   aArray.push(currentValue.text);
@@ -214,27 +230,27 @@ const replaceAnswerBtn = function () {
   while (aList.hasChildNodes()) {
     aList.removeChild(aList.firstChild);
   }
-  scoreBoard.classList.remove("incorrect", "correct");
+  scoreBoard.classList.remove('incorrect', 'correct');
   generateQuestionLetter();
   generateAnswer();
   shuffle(aArray);
   buildAnswerBtn();
 };
 
-span.addEventListener("click", function () {
+span.addEventListener('click', function () {
   replaceAnswerBtn();
-  modal.style.display = "none";
+  modal.style.display = 'none';
 });
 
-nBtn.addEventListener("click", function () {
+nBtn.addEventListener('click', function () {
   // generateModalLetter()
   if (reviewedIndex > 4) {
-    span.style.display = "block";
+    span.style.display = 'block';
     // When the user clicks on <span> (x), close the modal
     // When the user clicks anywhere outside of the modal, close it
-    window.addEventListener("click", function (event) {
+    window.addEventListener('click', function (event) {
       if (event.target == modal) {
-        modal.style.display = "none";
+        modal.style.display = 'none';
       }
     });
   }
@@ -242,6 +258,6 @@ nBtn.addEventListener("click", function () {
 
 //New letter generated
 //Generate new icon on "new" btn click"
-qBtn.addEventListener("click", replaceAnswerBtn);
+qBtn.addEventListener('click', replaceAnswerBtn);
 
 let stringScore = new String(currentScore);
