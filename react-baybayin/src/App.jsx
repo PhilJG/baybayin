8629c1276b3255a949a211d54845d9d646668477
Phil J Gray <phl.j.gray@gmail.com>
2023-09-26 15:46:06 -0700

    Added answer array buttons to react app

--- react-baybayin/src/App.jsx
+++ react-baybayin/src/App.jsx
@@ -1,19 +1,33 @@
 import { useState } from "react";
 import "./App.css";
 import data from "./assets/data.json";
-// import img from "./assets/imgs/";
 
 export default function App() {
   const [question, setQuestion] = useState(data[0].tiktik);
+  const [answerArray, setAnswerArray] = useState([]);
 
   //Randomly selects from an array
   const randomSelect = function (arr) {
     return Math.round(Math.random() * (arr.length - 1));
   };
 
+  function listItems() {
+    const updatedAnswerArray = [];
+    for (let i = 0; i < 4; i++) {
+      let index = randomSelect(data);
+      console.log("index " + index);
+      const randomAnswer = data[index];
+      randomAnswer["id"] = i;
+      console.log(`randomAnswer ${randomAnswer.letter}`);
+      updatedAnswerArray.push(randomAnswer);
+      console.log(updatedAnswerArray);
+    }
+    setAnswerArray(updatedAnswerArray);
+  }
   function handleRandomQuestion() {
-    let randomIndex = randomSelect(data);
-    setQuestion(data[randomIndex].tiktik);
+    let index = randomSelect(data);
+    setQuestion(data[index].tiktik);
+    listItems();
   }
 
   return (
@@ -26,6 +40,11 @@ export default function App() {
         Learn New Letters
       </button>
       <div className="question-text">{question}</div>
+      <ul className="answer-list">
+        {answerArray.map((a) => (
+          <li className="answer-item">{a.letter}</li>
+        ))}
+      </ul>
     </>
   );
 }
