1751a006251649911302e3029d635247d89c6cd9
Phil J Gray <phl.j.gray@gmail.com>
2023-09-26 14:13:35 -0700

    Added randomized question generation

--- react-baybayin/src/App.jsx
+++ react-baybayin/src/App.jsx
@@ -1,21 +1,41 @@
-import { Image } from "react";
-
+import { useState } from "react";
 import "./App.css";
+import data from "./assets/data.json";
+// import img from "./assets/imgs/";
 
 export default function App() {
+  const [question, setQuestion] = useState(data[0].tiktik);
+
+  //Randomly selects from an array
+  const randomSelect = function (arr) {
+    return Math.round(Math.random() * (arr.length - 1));
+  };
+
+  function handleRandomQuestion() {
+    let randomIndex = randomSelect(data);
+    setQuestion(data[randomIndex].tiktik);
+  }
+
   return (
     <>
       <h1>Baybayin</h1>
-      <button className="new-titik__btn">Learn New Letters</button>
-      <Titik />
+      <button
+        className="new-letter__btn"
+        onClick={() => handleRandomQuestion()}
+      >
+        Learn New Letters
+      </button>
+      <div className="question-text">{question}</div>
     </>
   );
 }
 
-function Titik() {
-  return (
-    <>
-      <Image src={`./assets/imgs/ba.svg`} />
-    </>
-  );
-}
+// function Letters() {
+//   return (
+//     <>
+//       {letters.map((el) => {
+//         <p>{el}</p>;
+//       })}
+//     </>
+//   );
+// }
