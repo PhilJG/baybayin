8906b7d312e9f8e69532db11c774d5acd0c422f6
Phil J Gray <phl.j.gray@gmail.com>
2023-09-26 20:07:13 -0700

    Added format state and applied to question component but not  answer buttons yet

--- react-baybayin/src/App.jsx
+++ react-baybayin/src/App.jsx
@@ -5,61 +5,84 @@ import data from "./assets/data.json";
 export default function App() {
   const [question, setQuestion] = useState("");
   const [answerArray, setAnswerArray] = useState([]);
+  const [questionFormat, setQuestionFormat] = useState(true);
 
   //Randomly selects from an array
   const randomSelect = function (arr) {
     return Math.round(Math.random() * (arr.length - 1));
   };
 
-  function listItems() {
+  //Sets the format format of the questions and ansers from baybayin(tiktik) & english(letter)
+  function handleQuestionFormat() {
+    const formatArray = [true, false];
+    const format = randomSelect(formatArray);
+    setQuestionFormat(formatArray[format]);
+    console.log(questionFormat);
+    return formatArray[format];
+  }
+
+  function handleAnswerList() {
     const updatedAnswerArray = [];
     for (let i = 0; i < 4; i++) {
       let index = randomSelect(data);
-      console.log("index " + index);
       const randomAnswer = data[index];
       randomAnswer["id"] = i;
-      console.log(`randomAnswer ${randomAnswer.letter}`);
       updatedAnswerArray.push(randomAnswer);
-      console.log(updatedAnswerArray);
     }
     setAnswerArray(updatedAnswerArray);
   }
+
   function handleRandomQuestion() {
     let index = randomSelect(data);
-    setQuestion(data[index].tiktik);
-    listItems();
+    const selectedFormat =
+      questionFormat == true ? data[index].tiktik : data[index].letter;
+    setQuestion(selectedFormat);
+    // listItems();
   }
 
   return (
     <>
       <h1>Baybayin</h1>
 
-      <Button onRandomQuestion={handleRandomQuestion} />
+      <Button
+        onQuestionFormat={handleQuestionFormat}
+        onRandomQuestion={handleRandomQuestion}
+        onAnswerList={handleAnswerList}
+      />
 
       <Question question={question} />
 
-      <AnswerList answerArray={answerArray} />
+      <AnswerList answerArray={answerArray} format={questionFormat} />
     </>
   );
 }
 
-function Button({ onRandomQuestion }) {
+function Button({ onQuestionFormat, onRandomQuestion, onAnswerList }) {
   return (
-    <button className="new-letter__btn" onClick={() => onRandomQuestion()}>
+    <button
+      className="new-letter__btn"
+      onClick={() => {
+        onQuestionFormat();
+        onRandomQuestion();
+        onAnswerList();
+      }}
+    >
       Learn New Letters
     </button>
   );
 }
 
-function Question({ question }) {
+function Question({ question, format }) {
   return <div className="question-text">{question}</div>;
 }
 
-function AnswerList({ answerArray }) {
+function AnswerList({ answerArray, format }) {
   return (
     <ul className="answer-list">
       {answerArray.map((a) => (
-        <button className="answer-item">{a.letter}</button>
+        <button className="answer-item" key={a.letter}>
+          {(format = true ? a.letter : a.tiktik)}
+        </button>
       ))}
     </ul>
   );
